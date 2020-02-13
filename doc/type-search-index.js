9f67cbe531179d8a81ef5f3ed95c69f55d79f254
ZestyTendy <57239266+ZestyTendy@users.noreply.github.com>
2020-02-12 22:03:14 -0600

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Distance"},{"p":"<Unnamed>","l":"Euclidean"},{"p":"<Unnamed>","l":"Hamming"},{"p":"<Unnamed>","l":"Tests"}]
